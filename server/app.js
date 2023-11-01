e2e0e6fcaf2d98319dd0071439cd44c8483bac4d
Chandra Prakash Joshi <chandraprakashjoshi@gmail.com>
2023-11-01 19:26:52 +0530

    Added CRUD biz logic for issue

--- server/app.js
+++ server/app.js
@@ -1,38 +1,55 @@
 const express = require("express");
 const cookieParser = require('cookie-parser')
-const tokenValidator = require("./tokenValidator");  
+const bodyParser = require("body-parser");
+const tokenValidator = require("./tokenValidator");
+const blobOperations = require("./blobStoreOperations");
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
 app.use(cookieParser());
+app.use(bodyParser.json());
 app.listen(PORT, () => console.log("Server started at port: " + PORT));
-
-const items = [
-    {
-        id: 1,
-        description: "Faulty flywheel",
-        priority: 3,
-        status: "Open"
-    },
-    {
-        id: 2,
-        description: "Missing door handle",
-        priority: 1,
-        status: "Assigned"
-    }
-];
 
 app.get("/api/issues", async (req, res) => {
     console.log(req.cookies);
     try {
-        var tokenValid = await tokenValidator.validateToken(req.headers.authorization.split(" ")[1]);        
-        console.log(tokenValid);
-        res.cookie("mycookie", "joshi");
-        res.send(items);    
+        //var tokenValid = await tokenValidator.validateToken(req.headers.authorization.split(" ")[1]);        
+        //console.log(tokenValid);
+        //res.cookie("mycookie", "joshi");
+        let items = await blobOperations.getIssues();
+        res.send(items);
     } catch (error) {
         res.status(401).send(error.message);
+    }
+})
+
+app.post("/api/issues", async (req, res) => {
+    console.log(req.body);
+    try {
+        let createdIssue = await blobOperations.createIssue(req.body);
+        res.send(createdIssue);            
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+})
+
+app.delete("/api/issues/:id", async (req, res) => {
+    try {
+        await blobOperations.deleteIssue(req.params.id);
+        res.send("Issue deleted successfully");
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+})
+
+app.get("/api/issues/:id", async (req, res) => {
+    try {
+        let issue = await blobOperations.getIssue(req.params.id);
+        res.send(issue);
+    } catch (error) {
+        res.status(404).send(error.message);
     }
 })
 
