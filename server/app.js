2b4aec537d07a05425616e52379c928d54f52b29
Chandra Prakash Joshi <chandraprakashjoshi@gmail.com>
2023-11-01 20:09:23 +0530

    Setting up ports for local development.

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const blobOperations = require("./blobStoreOperations");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cookieParser());
 app.use(bodyParser.json());
